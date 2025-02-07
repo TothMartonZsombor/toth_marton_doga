0ca6d0a1e8faec9ec17f2d5dc7b37225c77d4b45
TothMartonZsombor <77171552+TothMartonZsombor@users.noreply.github.com>
2025-02-07 09:42:37 +0100

    sdfhfg

--- database/factories/DestinationFactory.php
+++ database/factories/DestinationFactory.php
@@ -19,6 +19,8 @@ class DestinationFactory extends Factory
         return [
             //
             'destination' => fake()->randomElement(['A1','A2','A3','A4','A5','A6','A7','A8','A9','A10']),
+            'price' =>$this->faker->numberBetween(40000,80000),
+            'departure' => $this->faker->date(),
         ];
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,10 +27,12 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'role' =>fake()->randomElement(['admin', 'user']),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            
         ];
+        
     }
 
     /**
